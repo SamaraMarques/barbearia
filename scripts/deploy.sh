2e51f5e5e46834dfef8fe701e62df38b0d64c8b3
Thiago Leal <thfleal@gmail.com>
2021-03-18 23:26:38 -0300

    fix yaml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" kube/barbearia.yml
-cat kube/barbearia.yml
+sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" kube/barbearia.yaml
+cat kube/barbearia.yaml
