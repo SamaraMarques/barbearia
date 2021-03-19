9e60e476aa8404565511eab5b647ad1eace452ce
Thiago Leal <thfleal@gmail.com>
2021-03-18 23:07:15 -0300

    update

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
+app.use(express.static(path.join(__dirname, 'src/public')));
+
+app.get('/', async (req, res) => {
+  res.render('index');
+});
+
+app.get('/produtos', async (req, res) => {
+  res.render('produtos');
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log("Couldn't start Node server");
+  } else {
+    console.log('Sucessfully started Node server!');
+  }
+});
