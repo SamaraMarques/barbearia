d9ec2a6d7c1168e7347670885e764e80f7a21817
Thiago Leal <thfleal@gmail.com>
2021-03-19 12:04:25 -0300

    small change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
-sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" kube/barbearia.yaml
+# !/bin/sh
+# muda a versão de latest para a build do travis
+sed -i "s/latest/${TRAVIS_BUILD_NUMBER}/g" kube/barbearia.yaml
 cat kube/barbearia.yaml
