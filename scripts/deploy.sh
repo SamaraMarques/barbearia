fc638bfba206e33942db388fd5f10b740279dc45
Thiago Leal <thfleal@gmail.com>
2021-03-26 13:31:18 -0300

    fix script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 # !/bin/sh
 # muda a versão de latest para a build do travis
-sed -i "s/latest/${TRAVIS_BUILD_NUMBER}/g" kube/barbearia.yaml
-cat kube/barbearia.yaml
+sed -i "s/latest/${TRAVIS_BUILD_NUMBER}/g" kube/barbearia-app.yaml
+cat kube/barbearia-app.yaml
