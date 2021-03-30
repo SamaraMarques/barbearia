743f461e3a4329af381c9d797f688cc1e2f45054
Thiago Leal <thfleal@gmail.com>
2021-03-30 18:27:40 -0300

    fix file names

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,16 @@
-# !/bin/sh
-# muda a versão de latest para a build do travis
-sed -i "s/latest/${TRAVIS_BUILD_NUMBER}/g" kube/barbearia-app.yml
-cat kube/barbearia-app.yml
+#!/bin/sh
+#!/bin/bash
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+/usr/local/bin/kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  apply -f ./kube/
+echo "The build number is ${TRAVIS_BUILD_NUMBER}"
+/usr/local/bin/kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  set image deployment/barbearia-deployment barbearia-container=thileal/barbearia:${TRAVIS_BUILD_NUMBER} --record
